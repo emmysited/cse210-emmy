ad92c82f8ef710f254e30c0d8a6fb24c0777265b
emmysited <memmanuel1@byupathway.edu>
2025-05-09 16:05:16 +0300

    letter grade for a course

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,43 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("what is your grade percentage ?");
+        string answer = Console.ReadLine();
+        int percent = int.Parse(answer);
+
+        string letter ="";
+
+        if (percent >= 90)
+        {
+            letter ="A";
+        }
+        else if ( percent >=80)
+        {
+            letter ="B";
+        }
+        else if ( percent >=70)
+        {
+            letter ="c";
+        }
+        else if ( percent >=60)
+        {
+            letter ="D";
+        }
+        else
+        {
+            letter = "F";
+        }
+        Console.WriteLine($"your grade is:{letter}");
+
+        if (percent >=70)
+        {
+              Console.WriteLine("Congrats you have passed");
+        }
+      
+    
+        else
+        {
+            Console.WriteLine("Please try again!");
+        }
     }
 }
