6002ba740e3d01d38c3e19c1d18f89e883bbc3de
emmysited <memmanuel1@byupathway.edu>
2025-06-07 21:52:02 +0300

    b

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -4,6 +4,18 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+        // Create a list of shapes
+        List<Shape> shapes = new List<Shape>();
+
+        // Add various shapes to the list
+        shapes.Add(new Square("Red", 5));
+        shapes.Add(new Rectangle("Blue", 4, 6));
+        shapes.Add(new Circle("Green", 3));
+
+        // Display the area of each shape
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine($"Color: {shape.GetColor()}, Area: {shape.GetArea():F2}");
+        }
     }
 }
